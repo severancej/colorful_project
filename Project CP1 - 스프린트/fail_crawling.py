90188664965e9633054243bc59e578ac2d22e497
coding-jang <jje609@naver.com>
2022-11-09 23:10:19 +0900

    크크롤링

---
+++ Project CP1 - 스프린트/fail_crawling.py
@@ -0,0 +1,101 @@
+from urllib.request import urlopen   # urlopen 함수는 웹에서 얻은 데이터에 대한 객체를 반환해줍니다
+from urllib.parse import quote_plus  # 문자를 아스키코드로 변환하여 url에 넣기 위해
+from bs4 import BeautifulSoup   # beautifulsoup 사용
+from selenium import webdriver  # 웹드라이버 사용
+from selenium.webdriver.common.keys import Keys  # 웹에서 값을 입력해야할 때 사용
+import time
+import os  # 이미지를 저장할 폴더를 생성할때 필요
+
+
+# CERTIFICATE_VERIFY_FAILED 오류 해결
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# 아이디, 패스워드 입력받기
+# 인스타그램에서 검색을 하려면 반드시 로그인을 해야 사용가능하게끔 바꼈습니다..
+# 자동화를 위해 코드에 id와 pw를 넣으면 좋지만 보안상 input을 이용해 입력을 받아 구현하였습니다.
+id = input('instagram 아이디 및 계정 입력 : ')
+pw = input('instagram 패스워드 입력 : ')
+
+
+# 필요한 url설정
+baseUrl = 'https://www.instagram.com/explore/tags/'
+plusUrl = input("검색할 태그 입력 : ")
+url = baseUrl + quote_plus(plusUrl)  # quote_plus를 이용하여 아스키코드로 변환
+
+
+# 이미지를 저장할 img 폴더 생성 (폴더가 없을때만 생성)
+if not os.path.exists('./img'):
+    os.mkdir('./img')
+
+
+# 브라우저 실행 및 인스타그램 로그인 화면 이동
+driver = webdriver.Chrome()
+driver.get('https://www.instagram.com/accounts/login/')
+
+time.sleep(2)  # 웹드라이버가 웹 페이지를 로딩하는데 걸리는 시간을 기다려주기 위해 (>> Selenium의 단점..)
+
+
+# 로그인
+id_input = driver.find_element_by_css_selector(
+    '#loginForm > div > div:nth-child(1) > div > label > input')
+pw_input = driver.find_element_by_css_selector(
+    '#loginForm > div > div:nth-child(2) > div > label > input')
+
+id_input.send_keys(id)
+pw_input.send_keys(pw)
+pw_input.submit()
+
+time.sleep(4)
+
+
+# 로그인 후 뜨는 팝업창 해결
+save_late_button1 = driver.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/div/div/div/div/button')
+save_late_button1.click()
+
+driver.implicitly_wait(3)
+
+save_late_button2 = driver.find_element_by_xpath(
+    '/html/body/div[4]/div/div/div/div[3]/button[2]')
+save_late_button2.click()
+
+
+# 검색 페이지로 이동
+driver.get(url)
+time.sleep(1)
+
+
+# 드라이버로 페이지 소스를 가져와서 html 변수에 저장
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# 검색된 모든 인스타 게시물 선택(아래 selector를 가지고 있는 것을 불러와서 저장)
+posts = soup.select('._aabd._aa8k._aanf')  # 클래스가 여러개인 경우는 공백을 없애고 .으로 이어주기
+# posts 변수에는 .v1Nh3.kIKUG._bz0w 이 selector를 가지고 있는 모든 태그들이 들어감.
+
+
+# 각 게시물의 링크와 이미지 url가져오기
+for index, post in enumerate(posts):
+    print('https://www.instagram.com/' + post.a['href'])
+    imgUrl = post.select_one('._aagv').img['src']
+    with urlopen(imgUrl) as f:  # imgUrl을 열어서 저장
+
+        # 저장한 imgUrl을 다시 열어서 이미지 파일로 파일 이름을 지정해서 저장
+        # wb는 쓰기모드 + 바이너리모드. 이미지이기때문에 b모드를 써줘야한다.
+        with open('./img/' + plusUrl + str(index) + '.jpg', 'wb') as h:
+            image = f.read()  # f를 읽어와서 img라는 변수 안에 저장
+            h.write(image)  # 가져온 이미지를 해당 경로에 지정된 이름으로 저장
+
+    print(imgUrl)
+    print()
+
+
+# 코드 실행 잠시 멈춤
+time.sleep(2)
+
+
+# 드라이버 종료
+driver.quit()
---
+++ Project CP1 - 스프린트/instagram_crawling.py
@@ -0,0 +1,78 @@
+from urllib.request import urlopen
+# 아스키코드로 변환
+from urllib.parse import quote_plus as qp
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+# 스크롤을 위해 추가
+from selenium.webdriver.common.keys import Keys
+
+# 아래코드가 성공하면 시도할 url
+##baseurl = "https://www.instagram.com/leeji_ee/"
+baseurl = 'https://www.instagram.com/explore/tags/'
+plusurl = input('검색할 태그를 입력하세요 : ')
+url = baseurl + qp(plusurl)
+print(url)
+
+
+# 인스타그램은 다음 페이지를 클릭해서 추가로 보는 방식이 아니라
+# 스크롤을 내림으로써 이미지가 추가되는 방식이다.
+# 그래서 bs가 아닌 webdriver를 써야 한다.
+# 인스타페이지에서 우클릭으로 페이지소스보기하면 전부 자바스크립트(스크롤형식)로 쓰여있다.
+# 그래서 BeautifulSoup으로 불러올 수가 없다.
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(3)  # 딜레이 방지용
+
+# 스크롤 횟수 지정 예시
+# scroll_cnt = int(input("스크롤 횟수 : "))
+scroll_cnt = 33
+for i in range(scroll_cnt):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+time.sleep(3)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+# 클래스가 _aabd _aa8k _aanf 3개라서
+# 클래스 불러올거니깐 .을 붙이고 공간을 없앤다
+insta = soup.select('._aabd._aa8k._aanf')
+
+# 출력해보는게 중요하다. 작동이 안되는지 확인하기 위해서
+# print(insta)#클래스에 해당하는 인스타의 모든정보를 가져온다
+# print(insta[0])  # 다 가져오지 않고 첫 번째 정보만 가져온다
+
+# 1개만 가져오개 아니라 여러개를 가져올 거라서 반복문을 쓴다
+n = 1
+for i in insta:
+    # print(i.a['href'])
+    # href는 #(헤시태그)로 검색된 결과 중 해당 사진의 원주소를 말하는데
+    # 예시) /p/Ckcm89JPEiv/ 처럼 나오는데
+    # 이것만으로는 해당 페이지로 들어가기 어렵다.
+    # 그래서 인스타그램주소를 더한 형태로 만들어 준다.
+    print('https://www.instagram.com' + i.a['href'])
+
+    # 이미지가 속한 src태그가 해당하는 클래스(.aagv) 가져온다.
+    img_url = i.select_one('._aagv').img['src']  # 한개만 가져올거라 select_one()을 씀
+
+    # 이미지 저장하기
+    with urlopen(img_url) as f:
+        # 텍스트가 아니라서 w대신 wb씀
+        with open('C:/Users/coding-jang/Desktop/프로젝트/colorful_project/Project CP1 - 스프린트/img/' + plusurl + str(n) + '.jpg', 'wb') as h:
+            # 저장위치 : img폴더
+            # 파일명 : 검색어 + 순서,
+            #         확장자:.jpg
+            #         이미지 파일쓰기 : wb
+
+            img = f.read()  # url open한걸 읽고 img폴더에 저장
+            # h 에 img를 쓴다.
+            h.write(img)
+    # 반복하기 위해서
+    n += 1
+    print(img_url)
+    print()  # 출력된 결과를 읽기 편하기 위해 빈줄을 추가함
+
+
+driver.close()
